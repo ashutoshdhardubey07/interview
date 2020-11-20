c85d3a036b545e4133a94d9b4c8ca55189d97bc1
unknown <ashutoshd@crra.co>
2020-11-20 00:01:36 -0800

    dhwani_task

--- src/app/detail/detail-service.service.ts
+++ src/app/detail/detail-service.service.ts
@@ -3,13 +3,10 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable, Subject } from 'rxjs';
 import { environment } from 'src/environments/environment';
-// import {environment}
 @Injectable({
   providedIn: 'root'
 })
 export class DetailServiceService {
-  private pendingHTTPRequests$ = new Subject<void>();
-
   constructor(public http: HttpClient) { }
 
   getApi(url): Observable<any> {
